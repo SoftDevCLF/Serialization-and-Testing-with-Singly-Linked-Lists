ef38b15e7ee7bcd9b8bcddafe2a9b946c292d6f2
SoftDevCLF <cintya.lara.f@gmail.com>
2025-04-08 20:54:43 -0600

    Add Node and SSL classes

---
+++ Assignment3/Utility/SSL.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Utility
+{
+    public class SSL : ILinkedListADT
+    {
+        public void Add(User value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFirst(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddLast(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetValue(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Replace(User value, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
